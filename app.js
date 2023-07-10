c6472f39c3c67df1f779babdf31409aa8f4daf27
Sheyda Mousavi <121108864+3heyda@users.noreply.github.com>
2023-07-10 13:34:32 -0700

    Robot- L1
    This is V1 of JS Robot of our Team.

---
+++ app.js
@@ -0,0 +1,42 @@
+// Level 1 Robot
+
+//in the name of GOD
+//group name: Data Processors
+//members: (Sheyda , Marjan , Mohadese)
+
+// function robat(action) {
+//     if (action === 'سلام') {
+//         console.log('سلام به روی ماهت');
+//     }
+//     else if (action === 'Hello') {
+//         console.log('Hi');
+//     }
+//     else if (action === 'Salam') {
+//         console.log('Aleykome salam');
+//     }
+//     else if (action === 'اسمت چیه؟') {
+//         console.log('اسمم کوینه');
+//     }
+//     else if (action === 'What is your Name?') {
+//         console.log('My name is Kewin');
+//     }
+//     else if (action === 'esmet chiye?') {
+//         console.log('esmam kewine');
+//     }
+//     else if (action === 'چی میخوری؟') {
+//         console.log('هر چی تو بگی:)');
+//     }
+//     else {
+//         console.log('!!عزیزم دقت کن و مجدد سوالت رو بپرس')
+//     }
+// }
+
+// // //Outputs
+// robat('سلام');
+// robat('Hello');
+// robat('Salam');
+// robat('اسمت چیه؟')
+// robat('What is your Name?');
+// robat('esmet chiye?');
+// robat('چی میخوری؟');
+
