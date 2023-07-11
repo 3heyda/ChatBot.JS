0835b8012c2a28e952f538b7cb06b7d717ab8121
Sheyda Mousavi <121108864+3heyda@users.noreply.github.com>
2023-07-11 05:55:38 -0700

    use includes and querySelector

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Level 1 Robot
 
+
 //in the name of GOD
 //group name: Data Processors
 //members: (Sheyda , Marjan , Mohadese)
 
+// console.log("------------------روش 2-----------------");
 // function robat(action) {
 //     if (action === 'سلام') {
 //         console.log('سلام به روی ماهت');
@@ -40,3 +42,65 @@
 // robat('esmet chiye?');
 // robat('چی میخوری؟');
 
+// console.log("------------------روش 2-----------------");
+// function resPonse(question) {
+//     if (question = "سلام") {
+//         console.log(" عزیزم");
+//     } else if (question = "چطوری؟") {
+//         console.log("مگه دکتری؟");
+//     } else if (question == "چه خبر؟") {
+//         console.log("سلامتی");
+//     } else if (question == "اسمت چیه؟") {
+//         console.log("تربچه");
+//     } else if (question == "خونت کجاس؟") {
+//         console.log("تو باغچه");
+//     } else if (question == "چی میخوری؟") {
+//         console.log("آلوچه");
+//     } else if (question == "کلاس چندمی؟") {
+//         console.log("دانشگاه میرم");
+//     } else if (question == "هوا چطوره؟") {
+//         console.log("نمی دونم");
+//     } else {
+//         console.log("یکی بود یکی نبود در روزگار امروزی پادشاه وب جاوااسکریپت خود را کتک میزد و از رابی میترسید روزی پادشاه پایتون اعلام جنگ نمود و گفت بابا ولم کن");
+//     }
+// }
+
+// resPonse("q9")
+
+
+// Level 2
+// Shimobot
+
+
+
+let userInput = document.querySelector("#chatbotUserInput");
+
+userInput.addEventListener("change", function(){ 
+    bot(userInput.value)
+});
+
+function bot(resPonse) {
+
+    if (resPonse.includes("سلام", "سلام چطوری؟", "Chetori", "salam", "Salam", "Hello!", "Hi")) {
+        console.log("سلام و درود بر تو");
+    } else if (resPonse.includes("خوبی؟","","how are you")) {
+        console.log("مگه دکتری؟");
+    } else if (question == "چه خبر؟") {
+        console.log("سلامتی");
+    } else if (question == "اسمت چیه؟") {
+        console.log("تربچه");
+    } else if (question == "خونت کجاس؟") {
+        console.log("تو باغچه");
+    } else if (question == "چی میخوری؟") {
+        console.log("آلوچه");
+    } else if (question == "کلاس چندمی؟") {
+        console.log("دانشگاه میرم");
+    } else if (question == "هوا چطوره؟") {
+        console.log("نمی دونم");
+    } else {
+        console.log("یکی بود یکی نبود در روزگار امروزی پادشاه وب جاوااسکریپت خود را کتک میزد و از رابی میترسید روزی پادشاه پایتون اعلام جنگ نمود و گفت بابا ولم کن");
+    }
+
+}
+
+// bot("سلام")
